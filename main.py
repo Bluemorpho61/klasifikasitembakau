08713ba5081fdb25befb4402b4908efcd8f98c5e
Bluemorpho61 <vanda10.dava@gmail.com>
2025-05-27 21:33:54 +0700

    adding opencv library

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 img_width, img_height = 224, 224
 batch_size = 32
 
-# Create data generators for training and validation data
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -39,40 +38,32 @@ if train_generator.samples ==0:
 if validation_generator.samples ==0:
     raise ValueError("No images found in the validation directory")
 
-# Load pre-trained MobileNetV2 model
+
 base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 
-# Add custom classification layers
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x) # Adjust the number of units as needed
-# The line below is changed to have num_classes from the train_generator
+x = Dense(1024, activation='relu')(x) 
 predictions = Dense(train_generator.num_classes, activation='softmax')(x)
 
-# Create the final model
 model = Model(inputs=base_model.input, outputs=predictions)
 
-# Freeze the layers of the base model
 for layer in base_model.layers:
     layer.trainable = False
 
-# Compile the model
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
 model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
-    epochs=10, # Adjust the number of epochs as needed
+    epochs=10, 
     validation_data=validation_generator,
     validation_steps=validation_generator.samples // batch_size
 )
 
-# Save the model
 model.save('mobilenet_object_classification.h5')
 
 
-# Convert to TensorFlow Lite model (for Android)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 open("mobilenet_object_classification.tflite", "wb").write(tflite_model)
